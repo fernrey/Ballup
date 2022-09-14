89ab638cd2b876ba2a24ed0aeebae908d98b5657
fernrey <Fernando39@live.com>
2022-09-13 23:08:03 -0400

    some images for website

--- ballupbrian/src/Components/Home/index.js
+++ ballupbrian/src/Components/Home/index.js
@@ -1,8 +1,26 @@
-function Home () {
+import img from "../../Images/IMG_1112.jpg";
+import img2 from "../../Images/IMG_1312.jpg";
+import img3 from "../../Images/IMG_9111.jpg";
+import img4 from "../../Images/IMG_9375.jpg";
+
+import "./home.css";
+
+function Home() {
+    const images = [img, img2, img3, img4, img4, img2];
+
     return (
-        <div>hello this is the home page</div>
-
-
+        <div className="home-container">
+            <h1>Current</h1>
+            <div className="image-container">
+                {
+                    images.map((image) => (
+                        <div className="image-card">
+                            <img src={image} className="image" />
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
     );
 }
 
