878455aaf47d609c40702911e558d7d8863bc4b8
fernrey <Fernando39@live.com>
2022-12-19 20:40:32 -0500

    new changes

--- ballupbrian/src/Components/Home/index.js
+++ ballupbrian/src/Components/Home/index.js
@@ -2,7 +2,9 @@ import img from "../../Images/IMG_1112.jpg";
 import img2 from "../../Images/IMG_1312.jpg";
 import img3 from "../../Images/IMG_9111.jpg";
 import img4 from "../../Images/IMG_9375.jpg";
-
+import instagram from "../../Images/instagram.png";
+import tiktok from "../../Images/tiktok-icon.png";
+import linkedln from "../../Images/linkedln.png";
 import "./home.css";
 
 function Home() {
@@ -20,6 +22,23 @@ function Home() {
                     ))
                 }
             </div>
+            <section className="social-media">
+                <a href="https://www.instagram.com/ballupbrian/"
+                    target="_blank"
+                >
+                    <img src={instagram} alt="Instragram logo" className="socials ig" />
+                </a>
+                <a href="https://www.tiktok.com/@ballupbrian"
+                    target="_blank"
+                >
+                    <img src={tiktok} alt="tiktok logo" className="socials" />
+                </a>
+                <a href="https://www.linkedin.com/in/brian-ramos-822271201/"
+                    target="_blank"
+                >
+                    <img src={linkedln} alt="linkedln logo" className="socials" />
+                </a>
+            </section>
         </div>
     );
 }
